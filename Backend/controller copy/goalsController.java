68fddf626b58346ac33cae092ba82768d227f443
Harsh Patel <126726290+harshp425@users.noreply.github.com>
2024-01-22 21:57:39 -0500

    Update goalsController.java

--- Backend/controller copy/goalsController.java
+++ Backend/controller copy/goalsController.java
@@ -35,7 +35,7 @@ public class goalsController {
     @Autowired
     private GoalServiceImpl goalService;
 
-    private String secretKey = "@#HysfIFHk12jkghjghd42358JNFWY437468734FJJHgkJDGKJSkjhkjhvmnbU5345SDFBuemnchKJHUUjkjhkjhKJ2329KB45";
+    private String secretKey = "{Your Key}";
 
     public goalsController(Goals_Repository goalsRepository, GoalServiceImpl goalService) {
         this.goalsRepository = goalsRepository;
